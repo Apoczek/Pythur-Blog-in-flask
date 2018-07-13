9d75b75d4238a38e88b8bcb56a263ca1a53ffcaa
archi <apoczek@gmail.com>
2018-07-13 09:30:19 +0200

    Added routes for registration and login
    Added secret key

--- blog.py
+++ blog.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '28353652c1b05901379b1f9463dd87c2'
 
 
 posts = [
@@ -37,6 +41,18 @@ def home():
 @app.route("/O autorze")
 def about():
     return render_template('about.html')
+
+
+@app.route("/rejestracja")
+def registration():
+    form = RegistrationForm()
+    return render_template('register.html', title='Rejestracja', form=form)
+
+
+@app.route("/logowanie")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Logowanie', form=form)
 
 
 if __name__ == '__main__':
