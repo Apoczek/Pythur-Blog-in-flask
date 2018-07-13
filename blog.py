89388e5b3d3fb94a04754d6f0d8c31a5bf41565d
archi <apoczek@gmail.com>
2018-07-13 09:15:34 +0200

    Wprowadzenie formularzy 'forms'

--- blog.py
+++ blog.py
@@ -1,17 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Artur Kosior',
+        'title': 'Witam wszystkich w Pythur blog',
+        'content': ''' Witaj przybyszu. \n
+            Nazywam się Artur Kosior, a Ty jesteś na moim pierwszym "programistycznym" tworze. <br />
+            W 2017 roku postanowiłem wprowadzić kilka dużych zmian w moim życiu.
+            Jedną z nich jest nauka programowania. <br />
+            Do tej pory byłem związany z branżą spożywczą. Jakość, HACCP i tego typu klimaty.
+            Jednak powiedziałem sobie w końcu "dość" i oto jest efekt mojej decyzji.
+            Z czasem strona będzie ewoluować tak bardzo jak bardzo będzie się powiększać moja wiedza o programowaniu.
+            Nauki jest dużo. Na początek wszedł Python jako język backendowy teraz doszły HTML, CSS i JS co w małym stopniu widać tutaj. <br />
+            Miłego czytania i do zobaczenia już niedługo.''',
+        'date_posted': '10.07.2018'
+    },
+    {
+        'author': 'Jan Kowalski',
+        'title': 'Post numer 2',
+        'content': 'Zawartość drugiego posta',
+        'date_posted': '11.07.2018'
+    }
+
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Pythur blog</h1>"
+    return render_template('index.html', posts=posts)
 
 
-@app.route("/about")
+@app.route("/O autorze")
 def about():
-    return "<h1>O mnie</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Nazwa użytkownika', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    confirm_password = PasswordField('Potwierdź hasło', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Zarejestruj się')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    remember = BooleanField('Pamiętaj mnie')
+    submit = SubmitField('Zaloguj się')
